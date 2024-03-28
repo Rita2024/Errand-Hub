caa7011fe999ea733c95a6d6000e8b0976720ebb
Rita2024 <ritaejezie2@gmail.com>
2024-03-28 16:44:11 +0100

    updated the db.js file

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,11 @@
 // db.js
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');  // Importing mongoose library
 
-mongoose.connect('mongodb://localhost:27017/errand_hub', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect('mongodb://localhost:27017/errand_hub', {   // Connecting to MongoDB
+  useNewUrlParser: true,   // Option to use new URL parser
+  useUnifiedTopology: true,  // Option to use new Server Discover and Monitoring engine
 })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+.then(() => console.log('MongoDB connected')) // If connection is successful, log to console 
+.catch(err => console.log(err));  // If connection fails, log error to console
 
-module.exports = mongoose;
+module.exports = mongoose;  // Exporting mongoose object
