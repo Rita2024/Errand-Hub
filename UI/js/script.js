b037d1f839195e8a9080dc0e8e1923894db3ad58
Fabian <125877740+Fabianphilip@users.noreply.github.com>
2024-04-07 15:33:55 +0000

    updated js file

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,7 +1,7 @@
 const initSlider = () => {
     const imageList = document.querySelector(".slider-wrapper .image-list");
     const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
-    const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
+    const sliderScrollbar = document.querySelector(".container-c .slider-scrollbar");
     const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
     const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
 
